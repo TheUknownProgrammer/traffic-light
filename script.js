f9e8b4d285a793d34d7367a66f6e70119ed23de8
TheUknownProgrammer <iidcblazob@gmail.com>
2025-03-02 15:49:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 const traffic_light_components = document.querySelectorAll(".halogen-bulb");
 const interpretation = document.querySelectorAll(".interpretation");
 var previousLight = null;
+var currentLight = null;
+
+const car = {
+    element: document.getElementById("car"),
+    x: 0,
+    xVelocity: 0,
+    maxVelocity: 10,
+    drive: function() {
+        if(this.xVelocity < this.maxVelocity) {
+            this.xVelocity += 0.1;
+            this.xVelocity = parseFloat(this.xVelocity.toFixed(2));
+        }
+    },
+    slow: function() {
+        if(this.xVelocity >= this.maxVelocity/2) {
+        this.xVelocity -= 0.1;
+        this.xVelocity = parseFloat(this.xVelocity.toFixed(2));
+        }
+    },
+    stop: function() {
+        if(this.xVelocity > 0) {
+        this.xVelocity -= 0.1;
+        this.xVelocity = parseFloat(this.xVelocity.toFixed(2));
+        }
+    },
+    update: function() {
+        this.x += this.xVelocity;
+        this.element.style.left = this.x + "px";
+        if(this.x >= window.innerWidth) this.x = -this.element.offsetWidth;
+    }
+}
 
 traffic_light_components.forEach((component) => {
     component.addEventListener("click", function () {
-        if (previousLight != null)
-            previousLight.style.backgroundColor = "transparent";
+        currentLight = previousLight == component ? null : component;
+        if (previousLight != null) previousLight.style.backgroundColor = "transparent";
+
         component.style.backgroundColor =
             previousLight != component
                 ? component.getAttribute("data-color")
                 : "transparent";
+
         previousLight = previousLight != component ? component : null;
 
         interpretation.forEach(element => {
@@ -21,3 +54,16 @@ traffic_light_components.forEach((component) => {
 interpretation.forEach(element => {
     element.textContent = previousLight != null ? component.getAttribute("data-name") + "!" : "ERROR!";
 });
+
+function carDrive() {
+    if(currentLight != null) {
+        switch(currentLight.getAttribute("data-name")) {
+            case "GO": car.drive(); break;
+            case "SLOW": car.slow(); break;
+            case "STOP": car.stop(); break;
+        }
+        car.update();
+    }
+}
+
+setInterval(carDrive, 1000/60);
